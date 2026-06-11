20c640a32db9c6429481acd8520bb33767715286
Prathmesh <prathameshkumbhar2008@gmail.com>
2026-06-11 15:08:00 +0530

    Initial upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root"))(
+<BrowserRouter>
     <App />
   </BrowserRouter>
 );
